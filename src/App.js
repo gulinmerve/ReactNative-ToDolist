3f0df3ea29349577dcb665a667dab936df1c9978
Merve <gulineyel@gmail.com>
2020-10-15 18:41:12 +0300

    style remove

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 const App = (props) => {
   const [counter, setCounter] = useState(0);
   return (
-    <SafeAreaView style={{backgroundColor: '#37474F', flex: 1}}>
-      <View>
-        <Text>TODO</Text>
-        <Text>{counter}</Text>
+    <SafeAreaView style={style.container}>
+      <View style={style.todoView}>
+        <Text style={style.todo}>TODO</Text>
+        <Text style={style.todoCounter}>{counter}</Text>
+      </View>
+      <View style={style.addContainer}>
+        <TextInput style={style.inputContainer}></TextInput>
+        <TouchableOpacity style={style.addButton}>
+          <Text style={style.textButton}>ADD TODO</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
-export default App;
