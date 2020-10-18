841f0251af6a60332fc7321c32d670bc786cf19c
Merve <gulineyel@gmail.com>
2020-10-18 18:16:44 +0300

    main-todo connected

--- src/styles.js
+++ src/styles.js
@@ -1,23 +1,60 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+/**
+ * #303e45
+ * #a7b6bd
+ * #4a636e
+ * #ff9b31
+ */
+
 const main = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#303e45',
-  },
-  banner: {
-      flexDirection:"row",
-      justifyContent:"space-between",
-      padding:10,
-      alignItems:"center"
+    container: {
+        flex: 1,
+        backgroundColor: '#303e45'
     },
-  todoText: {
-      fontSize:50,
-      fontWeight:"bold",
-      color:"#ff9b31"
-  },
-  todoCount: {
-      fontSize:35,
-      color:"#ff9b31"
-  },
-});
-export default main;
+    banner: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        alignItems: 'center'
+    },
+    todoText: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: "#ff9b31"
+    },
+    todoCount: {
+        fontSize: 35,
+        color: "#ff9b31"
+    }
+})
+
+const todo_input = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#e0e0e0',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10
+    },
+    container: {
+        backgroundColor: '#a7b6bd',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5
+    },
+    buttonContainer: {
+        backgroundColor: '#4a636e',
+        padding: 10,
+        margin: 10,
+        width: Dimensions.get('window').width / 2.5,
+        alignSelf: 'center',
+        borderRadius: 8
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
+
+export { main, todo_input };
