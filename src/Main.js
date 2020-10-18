9d1c8a3372344bf455738b85effc6d199a663e90
Merve <gulineyel@gmail.com>
2020-10-18 18:39:07 +0300

    todo,

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,28 @@
 import React, {useState} from 'react';
-import {FlatList, SafeAreaView, Text, View, KeyboardAvoidingView} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 import {main} from './styles';
 import TodoInput from './components/TodoInput';
 
 const Main = () => {
   const [list, setList] = useState([]);
-  function addTodo(text) {
-    console.warn(text);
+  function addTodo() {
+    const element = {
+      id: list.length,
+      todo: Text,
+      // todo, Böyle de yazılabilir.
+      isDone: false,
+    };
   }
   return (
     <SafeAreaView style={main.container}>
-      <KeyboardAvoidingView style={main.container}>
+      <KeyboardAvoidingView behavior="padding" style={main.container}>
         <View style={main.banner}>
           <Text style={main.todoText}>TODO</Text>
           <Text style={main.todoCount}>10</Text>
@@ -25,7 +36,6 @@ const Main = () => {
 };
 
 export default Main;
-
 
 // Android için:
 // // <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS == 'android' ? null : 'padding'}>
