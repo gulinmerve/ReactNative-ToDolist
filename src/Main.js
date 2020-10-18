861dfcb0adf077a82a805425b040b63706b67d76
Merve <gulineyel@gmail.com>
2020-10-18 16:46:01 +0300

    TodoInput.js created

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import styles from './styles'
+import {SafeAreaView, Text, View} from 'react-native';
+import styles from './styles';
 /**
  * #303e45
  * #a7b6bd
  * #4a636e
  * #ff9b31
  */
-
 const Main = () => {
-    return (
-        <SafeAreaView style={ styles.container}>
-            <View style={styles.container}>
-
-            <View>
-                <Text>TODO</Text>
-                <Text>10</Text>
-                </View>
-            </View>
-        </SafeAreaView>
-    );
-}
-
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.banner}>
+          <Text style={styles.todoText}>TODO</Text>
+          <Text style={styles.todoCount}>10</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 export default Main;
