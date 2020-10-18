5ae016c747331b8a5e2f1f1403dbd682eebf3b3e
Merve <gulineyel@gmail.com>
2020-10-18 18:27:35 +0300

    KeyboardAvoidingView

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { FlatList, SafeAreaView, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, SafeAreaView, Text, View, KeyboardAvoidingView} from 'react-native';
 
-import { main } from './styles';
+import {main} from './styles';
 import TodoInput from './components/TodoInput';
 
 const Main = () => {
-    return (
-        <SafeAreaView style={main.container}>
-            <View style={main.container}>
-
-                <View style={main.banner}>
-                    <Text style={main.todoText}>TODO</Text>
-                    <Text style={main.todoCount}>10</Text>
-                </View>
-                {/* Flatlist Bulunduğu yeri kaplar */}
-                <FlatList
-                  data={[]}
-                  renderItem ={()=> null}
-                /> 
-                <TodoInput
-                    onTodoEnter={todoText => alert(todoText)}
-                />
-
-            </View>
-        </SafeAreaView>
-    );
-}
+  const [list, setList] = useState([]);
+  function addTodo(text) {
+    console.warn(text);
+  }
+  return (
+    <SafeAreaView style={main.container}>
+      <KeyboardAvoidingView style={main.container}>
+        <View style={main.banner}>
+          <Text style={main.todoText}>TODO</Text>
+          <Text style={main.todoCount}>10</Text>
+        </View>
+        {/* Flatlist Bulunduğu yeri kaplar */}
+        <FlatList data={[]} renderItem={() => null} />
+        <TodoInput onTodoEnter={(todoText) => addTodo(todoText)} />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
 
 export default Main;
+
+
+// Android için:
+// // <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS == 'android' ? null : 'padding'}>
+// //   <ScrollView style={{ flex: 1 }} bounces={false}>
+// //   </ScrollView>
+// // </KeyboardAvoidingView>
